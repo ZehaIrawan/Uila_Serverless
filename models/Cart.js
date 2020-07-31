ab2a29c8691eb12736c7ef7ab42ccffdd30aa413
Zeha Irawan <jangkaringbumi@gmail.com>
2020-07-31 15:56:59 +0700

    create cart endpoints

---
+++ models/Cart.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CartSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  products: [
+    {
+      product: {
+        type: Schema.Types.ObjectId,
+        ref: 'product'
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        default:0
+      },
+    },
+  ],
+  total: {
+    type: Number,
+    default:0
+  },
+  shipping: {
+    type: Number,
+    default:20,
+  },
+});
+
+module.exports = mongoose.model('cart', CartSchema);
